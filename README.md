e7bfa96478532a39b5cfd8ce92f86ee0aabbace1
Marcus <ghoshm@hotmail.co.uk>
2025-03-20 12:15:29 +0000

    Updated RNN model and added tutorial on temporal classifiers

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Evolving network architectures tutorial
+# Evolving network architectures tutorials
+
+A set of tutorials on evolving neural network architectures. 
+
+Note that some are in progress! 
--- src.py
+++ src.py
@@ -1,66 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 from tqdm import tqdm
+import copy
+
+plt.style.use("./style_sheet.mplstyle")
 
 
 class RecurrentNetwork:
     def __init__(self, n_inputs, n_hidden, n_outputs):
-        """"""
+        """
+        Creates an RNN model.
+        Arguments:
+            n_inputs: number of input units.
+            n_hidden: number of hidden units.
+            n_outputs: number of output units.
+        """
         # Units
         self.units = np.arange(n_inputs + n_hidden + n_outputs)
         self.unit_types = np.concatenate(
             [np.zeros(n_inputs), np.ones(n_hidden), np.ones(n_outputs) * 2]
-        )
-
-        # Activations
-        self.unit_activations = [np.zeros(len(self.units)), np.zeros(len(self.units))]
-        self.state = 0
+        )  # Input, hidden and output units are labelled as 0, 1, 2
 
         # Connections
-        self.connections = np.empty((0, 3))
+        self.connections = np.empty(
+            (0, 3)
+        )  # formatted as output unit, input unit, weight
 
     def forward(self, inputs):
-        """"""
+        """
+        Forward through the RNN model.
+        Arguments:
+            inputs: a numpy array (inputs x time).
+        """
         assert sum(self.unit_types == 0) == inputs.shape[0], "Mismatched inputs"
 
         # Reset network
-        self.unit_activations = [self.unit_activations[s] * 0.0 for s in [0, 1]]
-        outputs = []
+        self.activations_t_1 = np.zeros_like(self.units)
+        self.activations_t = np.zeros_like(self.units)
 
         # Run
         for time in range(inputs.shape[1]):
-            i_values = self.unit_activations[self.state]
-            o_values = self.unit_activations[1 - self.state]
-            self.state = 1 - self.state
 
             for unit in self.units:
-
                 if self.unit_types[unit] == 0:
-                    unit_input = inputs[unit, time]
+                    self.activations_t[unit] = inputs[unit, time]
                 else:
-                    unit_input = 0.0
+                    self.activations_t[unit] = 0
 
-                if len(self.connections) > 0:
+                if sum(self.connections[:, 1] == unit) > 0:
                     for o, i, w in self.connections[self.connections[:, 1] == unit]:
-                        unit_input += i_values[int(o)] * w
+                        self.activations_t[unit] += self.activations_t_1[int(o)] * w
 
-                o_values[unit] = 1 / (1 + np.exp(-unit_input))
+            self.activations_t_1 = np.copy(self.activations_t)
 
-            outputs.append(o_values[self.unit_types == 2])
-
-        return np.array(outputs)
-
-    def run_and_readout_spikes(self, inputs):
-        """"""
-
-        outputs = self.forward(inputs)
-        outputs += np.random.rand(*outputs.shape) / 1000
-        spikes = np.argwhere(outputs[:, 1] > outputs[:, 0]) * 0.1
-
-        return np.array(spikes).reshape(-1)
+        return self.activations_t[self.unit_types == 2] + np.random.normal(
+            loc=1e-9, scale=1, size=sum(self.unit_types == 2)
+        )
 
     def add_connections(self, number):
-        """"""
+        """
+        Add (number) connections to the RNN.
+            Note that only a single connection is permitted per unit pair.
+        """
 
         for _ in range(number):
             o = np.random.choice(self.units)
@@ -71,98 +73,94 @@ class RecurrentNetwork:
             self.connections = self.connections[idx]
 
     def add_nodes(self, number):
-        """"""
+        """
+        Add (number) nodes to the RNN.
+        """
 
         for _ in range(number):
             self.units = np.append(self.units, self.units[-1] + 1)
             self.unit_types = np.append(self.unit_types, 1)
-            self.unit_activations = [
-                np.zeros(len(self.units)),
-                np.zeros(len(self.units)),
-            ]
-            self.state = 0
 
 
-def plot_approximation(network, neuron_spike_times, neuron_spike_idx, input_I):
-    """"""
-    plt.subplots(figsize=(15, 5))
+# def plot_approximation(network, neuron_spike_times, neuron_spike_idx, input_I):
+#     """"""
+#     plt.subplots(figsize=(15, 5))
 
-    for idx_repeat in tqdm(range(10)):
-        neuron_spikes = neuron_spike_times[
-            neuron_spike_idx == idx_repeat
-        ]  # get "real" spikes
-        model_spikes = network.run_and_readout_spikes(
-            input_I[idx_repeat].reshape(1, -1)
-        )
+#     for idx_repeat in tqdm(range(10)):
+#         neuron_spikes = neuron_spike_times[
+#             neuron_spike_idx == idx_repeat
+#         ]  # get "real" spikes
+#         model_spikes = network.run_and_readout_spikes(
+#             input_I[idx_repeat].reshape(1, -1)
+#         )
 
-        plt.scatter(
-            neuron_spikes,
-            idx_repeat * np.ones(len(neuron_spikes)) + 0.2,
-            marker=".",
-            color="xkcd:purple",
-            label="HH model (spikes)" if idx_repeat == 0 else None,
-        )
-        plt.scatter(
-            model_spikes,
-            idx_repeat * np.ones(len(model_spikes)),
-            marker=".",
-            color="xkcd:dark seafoam green",
-            label="Network (spikes)" if idx_repeat == 0 else None,
-        )
+#         plt.scatter(
+#             neuron_spikes,
+#             idx_repeat * np.ones(len(neuron_spikes)) + 0.2,
+#             marker=".",
+#             color="xkcd:purple",
+#             label="HH model (spikes)" if idx_repeat == 0 else None,
+#         )
+#         plt.scatter(
+#             model_spikes,
+#             idx_repeat * np.ones(len(model_spikes)),
+#             marker=".",
+#             color="xkcd:dark seafoam green",
+#             label="Network (spikes)" if idx_repeat == 0 else None,
+#         )
 
-        plt.xlabel("Time")
-        plt.yticks([])
-        plt.ylabel("Repeat")
-        plt.legend()
-
-
-def van_rossum_distance(t0, t1, duration, tau_vr=5, dt=0.1):
-    """"""
-    n = int(np.round(duration / dt))
-    x0 = np.zeros(n)
-    x1 = np.zeros(n)
-    for x, t in [(x0, t0), (x1, t1)]:
-        x[np.array(np.round(t / dt), dtype=int)] = 1
-    nk = int(np.round(3 * tau_vr / dt))
-    if 2 * nk + 1 > n:
-        nk = (n - 1) // 2
-    T = np.arange(-nk, nk + 1) * dt
-    kernel = np.exp(-T / tau_vr) / tau_vr
-    for x in [x0, x1]:
-        x[:] = np.convolve(x, kernel, "same")
-
-    return np.sqrt(np.sum((x0 - x1) ** 2 * dt) / tau_vr)
+#         plt.xlabel("Time")
+#         plt.yticks([])
+#         plt.ylabel("Repeat")
+#         plt.legend()
 
 
-def mean_vr_distance(
-    network,
-    neuron_spike_times,
-    neuron_spike_idx,
-    input_I,
-    dt=0.1,
-    disable_reporting=True,
-):
-    """"""
-    duration = input_I.shape[1] * dt
+# def van_rossum_distance(t0, t1, duration, tau_vr=5, dt=0.1):
+#     """"""
+#     n = int(np.round(duration / dt))
+#     x0 = np.zeros(n)
+#     x1 = np.zeros(n)
+#     for x, t in [(x0, t0), (x1, t1)]:
+#         x[np.array(np.round(t / dt), dtype=int)] = 1
+#     nk = int(np.round(3 * tau_vr / dt))
+#     if 2 * nk + 1 > n:
+#         nk = (n - 1) // 2
+#     T = np.arange(-nk, nk + 1) * dt
+#     kernel = np.exp(-T / tau_vr) / tau_vr
+#     for x in [x0, x1]:
+#         x[:] = np.convolve(x, kernel, "same")
 
-    # Convert (t,i) format to list
-    neuron_spikes, model_spikes = [], []
-    for idx_repeat in tqdm(range(input_I.shape[0]), disable=disable_reporting):
+#     return np.sqrt(np.sum((x0 - x1) ** 2 * dt) / tau_vr)
 
-        # "Real" neuron
-        neuron_spikes.append(neuron_spike_times[neuron_spike_idx == idx_repeat])
+# def mean_vr_distance(
+#     network,
+#     neuron_spike_times,
+#     neuron_spike_idx,
+#     input_I,
+#     dt=0.1,
+#     disable_reporting=True,
+# ):
+#     """"""
+#     duration = input_I.shape[1] * dt
 
-        # Model
-        m_spikes = network.run_and_readout_spikes(input_I[idx_repeat].reshape(1, -1))
-        model_spikes.append(m_spikes)
+#     # Convert (t,i) format to list
+#     neuron_spikes, model_spikes = [], []
+#     for idx_repeat in tqdm(range(input_I.shape[0]), disable=disable_reporting):
 
-    # Distance
-    d = 0
-    for t0, t1 in zip(neuron_spikes, model_spikes):
-        d += van_rossum_distance(t0, t1, duration, dt=dt)
-    d /= len(neuron_spikes)
+#         # "Real" neuron
+#         neuron_spikes.append(neuron_spike_times[neuron_spike_idx == idx_repeat])
 
-    if disable_reporting is False:
-        print("Mean vr distance: " + str(np.round(d, 2)))
+#         # Model
+#         m_spikes = network.run_and_readout_spikes(input_I[idx_repeat].reshape(1, -1))
+#         model_spikes.append(m_spikes)
 
-    return d
+#     # Distance
+#     d = 0
+#     for t0, t1 in zip(neuron_spikes, model_spikes):
+#         d += van_rossum_distance(t0, t1, duration, dt=dt)
+#     d /= len(neuron_spikes)
+
+#     if disable_reporting is False:
+#         print("Mean vr distance: " + str(np.round(d, 2)))
+
+#     return d
